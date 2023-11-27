a1db2a8693d7a26ed2f79ea29499fcae3830f22d
djgamertri <djgamertri@gmail.com>
2023-11-27 11:25:08 -1200

    Proyecto final terminado

--- models/userModel.js
+++ models/userModel.js
@@ -1,3 +1,4 @@
+const bcryptjs = require('bcrypt')
 const mongoose = require('mongoose')
 
 //Definir un modelo que trabaje para solo Mongo
@@ -25,6 +26,18 @@ const userSchema = new mongoose.Schema({
         maxlenght: [20, "La contraseña solo debe tener 20 caracteres"]
     }
 })
+
+userSchema.pre('save', async function(next){
+    //generar la sal para la password
+    const sal = await bcryptjs.genSalt(10)
+    //crear clave encriptada con sal y asignar password a la entidad
+    this.Password = await bcryptjs.hash(this.Password , sal)
+
+})
+
+userSchema.methods.compararPassword = async function(Password){
+    return await bcryptjs.compare(Password, this.Password)
+}
 
 const User = mongoose.model( "User", userSchema )
 module.exports = User
--- routes/usersRoutes.js
+++ routes/usersRoutes.js
@@ -77,6 +77,62 @@ router.post('/' , async (req , res) => {
     }    
 })
 
+//REGISTER
+router.post('/register' , async (req , res) => {
+
+    try {
+        const User = await userModel.create(req.body)
+        res.status(201).json({
+            success:true,
+            data: User
+        })
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            error: `${error.mensage}`
+        })
+    }    
+})
+
+//LOGIN
+router.post('/login' , async (req , res) => {
+
+    try {
+        //Separar email y password
+        const {email, Password} = req.body
+
+        //buscar usuario con email
+        const User = await userModel.findOne({email}).select("+Password")
+        console.log(User)
+        if(!User){
+            res.status(404).json({
+                success: false,
+                msg: 'User not found'
+            })
+        } else {
+            //Comparar Password
+            const isMatch = await User.compararPassword(Password)
+            if(isMatch){
+                res.status(200).json({
+                    success:true,
+                    msg: 'Usuario logeado correctamente'
+                })
+            } else {
+                res.status(401).json({
+                    success:false,
+                    msg:'Credenciales invalidas'
+                })
+            }
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            success:false,
+            msg: `Error interno del servidor ${error.mensage}`
+        })
+    }    
+})
+
 //EDITAR
 router.put('/:id' , async (req , res) => {
     try {
